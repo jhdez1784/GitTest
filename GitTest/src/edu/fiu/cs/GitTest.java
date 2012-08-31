22c07b12030be686618bce70d2c86fc29f76d305
Julio <Julio@Julio-PC.codebell.ad>
2012-08-31 11:04:49 -0400

    First Commit

---
+++ GitTest/src/edu/fiu/cs/GitTest.java
@@ -0,0 +1,13 @@
+package edu.fiu.cs;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a GitHub Test!!!");
+	}
+
+}
